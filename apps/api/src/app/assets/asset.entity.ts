cee846d6f1c43cd8b9370405e7bb72ad7db03943
helfie-kovalov <Serhii.Kovalov@helfie.ai>
2024-11-01 17:29:39 +0200

    feat(api): assets

--- apps/api/src/app/assets/asset.entity.ts
+++ apps/api/src/app/assets/asset.entity.ts
@@ -2,17 +2,23 @@ import { ApiProperty } from "@nestjs/swagger";
 import { BelongsTo, Column, DataType, ForeignKey, HasMany, Model, PrimaryKey, Table } from "sequelize-typescript";
 import { User } from "../users/user.entity";
 import { Obligation } from "../obligations/obligation.entity";
+import { TokenIdentity } from "../token-identities/token-identity.entity";
+import { TokenClaim } from "../token-claims/token-claim.entity";
 
 @Table
 export class Asset extends Model<Asset> {
     @PrimaryKey
-    @Column({type: DataType.INTEGER, autoIncrement: true, allowNull: false})
-    @ApiProperty({type: Number, example: 0})
-    id: number;
+    @Column({type: DataType.STRING, allowNull: false})
+    @ApiProperty({type: String, example: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"})
+    tokenAddress: string;
+    @ForeignKey(() => TokenIdentity)
+    @Column({type: DataType.STRING, allowNull: true})
+    @ApiProperty({type: String, example: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"})
+    identityAddress: string;
     @ForeignKey(() => User)
     @Column({type: DataType.STRING, allowNull: false})
     @ApiProperty({type: String, example: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"})
-    userAddress: string;
+    deployer: string;
     @ForeignKey(() => Obligation)
     @Column({type: DataType.INTEGER, allowNull: true})
     @ApiProperty({type: Number, example: 0})
@@ -26,8 +32,15 @@ export class Asset extends Model<Asset> {
     @Column({type: DataType.STRING, allowNull: false})
     @ApiProperty({type: String, example: "RWA test asset"})
     type: string;
+    @Column({type: DataType.BOOLEAN, allowNull: false, defaultValue: false})
+    @ApiProperty({type: Boolean, example: false})
+    isVerified: boolean;
     @BelongsTo(() => User)
-    user: User
+    user: User;
     @BelongsTo(() => Obligation)
-    obligation: Obligation
+    obligation: Obligation;
+    @HasMany(() => TokenClaim)
+    claims: TokenClaim[];
+    @BelongsTo(() => TokenIdentity)
+    identity: TokenIdentity;
 }
