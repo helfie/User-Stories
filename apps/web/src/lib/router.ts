61b094434b253a62203083d4566933f184ddd997
helfie-kovalov <Serhii.Kovalov@helfie.ai>
2024-11-03 02:03:56 +0200

    feat(web): router

--- apps/web/src/lib/router.ts
+++ apps/web/src/lib/router.ts
@@ -5,11 +5,15 @@ import { MarketPage } from '../pages/market-page';
 import { HomePage } from '../pages/home-page';
 import { AssetPage } from '../pages/asset-page';
 import { AdminUserPage } from '../pages/admin-user-page';
+import { AdminAssetPage } from '../pages/admin-asset-page';
+import { AdminAssetClaimPage } from '../pages/admin-asset-claim-page';
 
 export const routes = {
     home: '/',
     adminClaim: '/admin-claim',
     adminUser: '/admin-user',
+    adminAssetClaim: '/admin-asset-claim',
+    adminAsset: '/admin-asset',
     market: '/market',
     asset: '/asset',
 }
@@ -20,6 +24,8 @@ export const router = createBrowserRouter([{
     },
     { path: routes.adminClaim, Component: AdminClaimPage },
     { path: routes.adminUser, Component: AdminUserPage },
+    { path: routes.adminAssetClaim, Component: AdminAssetPage },
+    { path: routes.adminUser, Component: AdminAssetClaimPage },
     { path: routes.market, Component: MarketPage },
     { path: routes.asset, Component: AssetPage },
     ]
