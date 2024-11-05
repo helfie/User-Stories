7ca3d57a066c1c8fa79267183382c4568296c9f8
kovalov-helfie <Serhii.Kovalov@helfie.ai>
2024-11-05 23:36:55 +0200

    fix(web): router

--- apps/web/src/lib/router.ts
+++ apps/web/src/lib/router.ts
@@ -6,27 +6,33 @@ import { HomePage } from '../pages/home-page';
 import { AssetPage } from '../pages/asset-page';
 import { AdminUserPage } from '../pages/admin-user-page';
 import { AdminAssetPage } from '../pages/admin-asset-page';
-import { AdminAssetClaimPage } from '../pages/admin-asset-claim-page';
+import { AdminTokenCompliancePage } from '../pages/admin-token-compliance-request-page';
+import { AddAssetClaimsPage } from '../pages/add-asset-claims-page';
+import { AddTokenComplianceRequestPage } from '../pages/add-token-compliance-request-page';
 
 export const routes = {
     home: '/',
     adminClaim: '/admin-claim',
     adminUser: '/admin-user',
-    adminAssetClaim: '/admin-asset-claim/:tokenAddress',
     adminAsset: '/admin-asset',
-    market: '/market',
+    adminAssetClaim: '/admin-asset-claim/:tokenAddress',
+    adminTokenCompliance: 'admin-token-compliance',
     asset: '/asset',
+    market: '/market',
+    assetClaim: 'asset-claim/:tokenAddress',
+    tokenComplainceRequest: 'token-compliance-request/:tokenAddress-:userAddress',
 }
 
 export const router = createBrowserRouter([{
-    path: routes.home, Component: DefaultLayout, children: [{
-        path: routes.home, Component: HomePage
-    },
-    { path: routes.adminClaim, Component: AdminClaimPage },
-    { path: routes.adminUser, Component: AdminUserPage },
-    { path: routes.adminAsset, Component: AdminAssetPage },
-    { path: routes.adminAssetClaim, Component: AdminAssetClaimPage },
-    { path: routes.market, Component: MarketPage },
-    { path: routes.asset, Component: AssetPage },
+    path: routes.home, Component: DefaultLayout, children: [
+        { path: routes.home, Component: HomePage },
+        { path: routes.adminClaim, Component: AdminClaimPage },
+        { path: routes.adminUser, Component: AdminUserPage },
+        { path: routes.adminAsset, Component: AdminAssetPage },
+        { path: routes.adminTokenCompliance, Component: AdminTokenCompliancePage },
+        { path: routes.asset, Component: AssetPage },
+        { path: routes.market, Component: MarketPage },
+        { path: routes.assetClaim, Component: AddAssetClaimsPage },
+        { path: routes.tokenComplainceRequest, Component: AddTokenComplianceRequestPage },
     ]
 }])
